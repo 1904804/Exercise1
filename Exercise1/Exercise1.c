70bc8426e9e2d3a0e7c4a5436517c2555c94ea3f
Julian Milligan <56824914+1904804@users.noreply.github.com>
2020-03-01 15:45:59 +0000

    Final as off 01/03/2020

--- Exercise1/Exercise1.c
+++ Exercise1/Exercise1.c
@@ -7,10 +7,11 @@
 //Create - populate - display 3 variables.
 void main()
 {
+	//Declare variables and set memory.
 	int cartAmount;
 	char bookTitle[50];
-	int bookTitleLenght;
-	float lenght;
+	int bookTitlelength;
+	float length;
 
 	printf_s("Please enter the number of items in your shopping cart - \n");
 	scanf_s("%d" , &cartAmount);//capture the amount 
@@ -22,28 +23,30 @@ void main()
 
 	}
 
-	printf_s("Please enter a title of a great book - \n");//capture book title
-	scanf_s(" %c", bookTitle);
-	printf_s("Book lenght = %d \n", strlen(bookTitle));//***get advise 
+	printf_s("Please enter a title of a great book ( please use _ as a space ) - \n");//capture book title
+	scanf_s("%s" , bookTitle , strlen(bookTitle));
+	bookTitlelength = strlen(bookTitle);//not able to caputre spaces
+	//printf_s("Book length = %d \n", strlen(bookTitle));//Testing to see if book length is caputered
 
-	printf_s("Please enter the lenght you wish to save - \n");
-	scanf_s("%f", lenght);
+
+	printf_s("Please enter the length you wish to save ( must be between 0cm and 100cm ) - \n");//caputre length user wishes to save
+	scanf_s("%f", &length);
 	
+	
+	while (length <= 0 | length > 100)//loop to make sure value is with the range
+	{
+		printf_s("Please enter a value between 0cm and 100cm - \n");
+		scanf_s(" %f", &length);
 
-
-
-
+	}
 
 
 	//Display the values to the user
 	printf_s("You have %d items in your cart.\n", cartAmount);//cart value
-	printf_s("The title you entered was - %s\n", bookTitle);//book title value  , **Get advise
-	printf_s("The value you wished to save was - %f", lenght);
+	printf_s("The title you entered was - %s\n", bookTitle);//book title value 
+	printf_s("The value you wished to save was - %.2fcm\n", length);
 
 
 }
-
-
-
 
 
